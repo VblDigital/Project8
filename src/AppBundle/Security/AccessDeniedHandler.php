73e7ee13e64cefe620f1869eda47f2f78db3996f
vbopenclass <vbopenclass@gmail.com>
2020-02-14 23:26:12 +0100

    Authentication(Users)Restricted to ROLE_ADMIN

---
+++ src/AppBundle/Security/AccessDeniedHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace AppBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $message = "L'accès à cette page est restreint";
+
+        return new Response($message, 403);
+    }
+}
