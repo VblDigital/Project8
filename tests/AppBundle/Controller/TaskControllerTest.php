e020ba75ed6f4a13d3eed7d2d480138b60727b5d
vbopenclass <42405431+vbopenclass@users.noreply.github.com>
2020-02-20 21:51:16 +0100

    Performance (#19)
    * Relation(User&Task)Added ManyToOne/OneToMany between User and Task
    
    * Roles(User)Added roles : admin&user
    
    * PhpUnit(User)Added unit tests for User entity
    
    * Relation(User&Task)Added ManyToOne/OneToMany between User and Task
    
    * Roles(User)Added roles : admin&user
    
    * PhpUnit(User)Added unit tests for User entity
    
    * Phpunit(Task)Added unit tests for task
    
    * Travis
    
    * Relation(User&Task)Added ManyToOne/OneToMany between User and Task
    
    * Roles(User)Added roles : admin&user
    
    * Performance(Tests)User+Task+Security

---
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskControllerTest extends WebTestCase
+{
+    public function setUp()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'Test',
+            'PHP_AUTH_PW'   => 'test',
+        ]);
+
+        return $client;
+    }
+
+    public function testTaskListActionIfNotLogged()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateTaskIfLogged()
+    {
+        $client = $this->setUp();
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form([
+            'task[title]' => 'New title',
+            'task[content]' => 'New content',
+        ]);
+
+        $client->submit($form);
+        $this->assertSame(
+            302,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $crawler = $client->followRedirect();
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testEditTaskIfLogged()
+    {
+        $client = $this->setUp();
+        $crawler = $client->request('GET', '/tasks/83/edit');
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Modifier')->form([
+            'task[title]' => 'New title for the Task',
+        ]);
+        $client->submit($form);
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testToogleTaskActionIfLogged()
+    {
+        $client = $this->setUp();
+        $client->request('GET', '/tasks/83/toggle');
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testDeleteTaskIfLogged()
+    {
+        $client = $this->setUp();
+        $client->request('GET', '/tasks/83/delete');
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+}
